cd809bfc7ba53059edf1bd1973094e384d8b5902
Agung Firmansyah <agungfir98@gmail.com>
2022-10-06 13:41:08 +0700

    index dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,26 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import { useState, useCallback } from "react";
+import { Dashboard } from "../components/dashboard";
+import { Navbar } from "../components/navbar";
+import { SlideMenu } from "../components/sideNav";
 
 const Home: NextPage = () => {
+  const [sideNavOpen, setSideNavOpen] = useState<boolean>(false);
+
+  const handleNav = () => {
+    setSideNavOpen(!sideNavOpen);
+  };
+
   return (
-    <>
-      <div className="container">a</div>
-    </>
+    <div className="max-h-screen bg-[#F6F6F9] overflow-hidden">
+      <Navbar navOpen={sideNavOpen} setNavOpen={handleNav} />
+      <div className="h-[calc(100vh-73px)] flex">
+        <SlideMenu navOpen={sideNavOpen} setNavOpen={handleNav} />
+        <main className="p-5 py-6 h-full grow overflow-auto">
+          <Dashboard />
+        </main>
+      </div>
+    </div>
   );
 };
 

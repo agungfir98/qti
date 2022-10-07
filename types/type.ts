a4575d914358ea0a51d3d2c83268b7225e263b80
Agung Firmansyah <agungfir98@gmail.com>
2022-10-07 23:45:12 +0700

    activePat and UsersData type

--- types/type.ts
+++ types/type.ts
@@ -12,8 +12,21 @@ export interface credentials {
   token: string;
 }
 
-export enum activeState {
-  DASHBOARD = "dashboard",
-  SALES = "sales",
-  USER_MNG = "user-management",
+export enum activePath {
+  DASHBOARD = "/",
+  SALES = "/sales",
+  USER = "/user",
+}
+
+export interface UsersData {
+  count: number;
+  page_count: number;
+  page_size: number;
+  page: number;
+  results: {
+    id: string;
+    employee: string;
+    email: string;
+    is_active: boolean;
+  }[];
 }

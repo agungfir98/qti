be74841d862bef62cb8337638a674580a56af951
Agung Firmansyah <agungfir98@gmail.com>
2022-10-07 15:38:19 +0700

    login functionality

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,9 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 import { FormEvent, HTMLInputTypeAttribute, useEffect, useState } from "react";
-import { loginForm } from "../types/type";
+import { credentials, loginForm } from "../types/type";
+import { api } from "../utils/api";
+import { setCredentials } from "../utils/credentials";
 
 const LoginPage: NextPage = () => {
   const [form, setForm] = useState<loginForm>({
@@ -8,6 +11,14 @@ const LoginPage: NextPage = () => {
     password: "",
     rememberMe: false,
   });
+  const [cred, setCred] = useState<credentials | string>((): any => {
+    if (typeof window !== "undefined")
+      return window.localStorage.getItem("creds");
+  });
+  const [errorMsg, setErrorMsg] = useState<[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const router = useRouter();
 
   const handleChange = (e: {
     target: { value: HTMLInputTypeAttribute; name: HTMLInputTypeAttribute };
@@ -39,83 +50,109 @@ const LoginPage: NextPage = () => {
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    console.log("loginnnnn");
-    console.log(form);
+    api
+      .post("/auth/login", {
+        email: form.email,
+        password: form.password,
+      })
+      .then((res: { data: credentials }) => {
+        setCredentials(res.data);
+        window.localStorage.setItem("creds", JSON.stringify(res.data));
+        router.push("/");
+      })
+      .catch((err) => {
+        const { status } = err.response;
+        if (status === 422) {
+          console.error(err);
+          setErrorMsg(() => err.detail.map((v: { loc: [] }) => v.loc));
+        }
+        if (status === 400) {
+          console.error(err.message);
+        }
+        status === 500 && console.error(err);
+      });
   };
 
   useEffect(() => {
-    console.log(form);
-  }, [form]);
+    cred && router.push("/");
+    !cred && setLoading(false);
+
+    () => setLoading(false);
+  }, [cred, router]);
 
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-tr from-cyan-400 to-blue-500">
-      <div className="flex flex-col gap-3 w-full h-fit m-[18px] md:w-[467px] bg-white rounded-xl p-[18px] py-7 shadow-customsatu">
-        <header className="flex flex-col gap-2 justify-center">
-          <h1 className="text-center text-[#03050B] text-xl font-semibold">
-            Login
-          </h1>
-          <p className="text-center text-[rgba(52,52,52,0.7)] font-medium text-sm">
-            Welcome back, enter your credentials to continue
-          </p>
-        </header>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-          {/* // *Email ======== */}
-          <div className="w-full flex flex-col gap-1">
-            <label htmlFor="email" className="font-semibold text-sm">
-              Email
-            </label>
-            <input
-              id="email"
-              name="email"
-              type="text"
-              value={form.email}
-              onChange={handleChange}
-              placeholder="Enter email"
-              className="bg-[#EFEFEF] p-3 px-4 rounded-xl"
-            />
-          </div>
+    <>
+      {!loading && (
+        <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-tr from-cyan-400 to-blue-500">
+          <div className="flex flex-col gap-3 w-full h-fit m-[18px] md:w-[467px] bg-white rounded-xl p-[18px] py-7 shadow-customsatu">
+            <header className="flex flex-col gap-2 justify-center">
+              <h1 className="text-center text-[#03050B] text-xl font-semibold">
+                Login
+              </h1>
+              <p className="text-center text-[rgba(52,52,52,0.7)] font-medium text-sm">
+                Welcome back, enter your credentials to continue
+              </p>
+            </header>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+              {/* // *Email ======== */}
+              <div className="w-full flex flex-col gap-1">
+                <label htmlFor="email" className="font-semibold text-sm">
+                  Email
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="text"
+                  value={form.email}
+                  onChange={handleChange}
+                  placeholder="Enter email"
+                  className="bg-[#EFEFEF] p-3 px-4 rounded-xl"
+                />
+              </div>
 
-          {/* // *Password ======== */}
-          <div className="w-full flex flex-col gap-1">
-            <label htmlFor="password" className="font-semibold text-sm">
-              Password
-            </label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              value={form.password}
-              onChange={handleChange}
-              placeholder="Enter password"
-              className="bg-[#EFEFEF] p-3 px-4 rounded-xl"
-            />
-          </div>
+              {/* // *Password ======== */}
+              <div className="w-full flex flex-col gap-1">
+                <label htmlFor="password" className="font-semibold text-sm">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={form.password}
+                  onChange={handleChange}
+                  placeholder="Enter password"
+                  className="bg-[#EFEFEF] p-3 px-4 rounded-xl"
+                />
+              </div>
 
-          {/* // *CheckBox ======== */}
-          <div className=" flex gap-2 my-1 items-center">
-            <input
-              type="checkbox"
-              name="rememberme"
-              id="rememberme"
-              checked={form.rememberMe}
-              onChange={handleChange}
-            />
-            <label
-              htmlFor="rememberme"
-              className="font-semibold text-sm cursor-pointer checkbox-label flex"
-            >
-              Remember me
-            </label>
+              {/* // *CheckBox ======== */}
+              <div className=" flex gap-2 my-1 items-center">
+                <input
+                  type="checkbox"
+                  name="rememberme"
+                  id="rememberme"
+                  checked={form.rememberMe}
+                  onChange={handleChange}
+                />
+                <label
+                  htmlFor="rememberme"
+                  className="font-semibold text-sm cursor-pointer checkbox-label flex"
+                >
+                  Remember me
+                </label>
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-[#198564] rounded-lg py-3 text-white font-semibold"
+              >
+                Login
+              </button>
+            </form>
           </div>
-          <button
-            type="submit"
-            className="w-full bg-[#198564] rounded-lg py-3 text-white font-semibold"
-          >
-            Login
-          </button>
-        </form>
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 export default LoginPage;

0ff026aaa1df58c55b1756e21be124a336bbe81c
Agung Firmansyah <agungfir98@gmail.com>
2022-10-07 15:41:31 +0700

    add authentication validation

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ import { Navbar } from "../components/navbar";
 import SalesComponent from "../components/sales";
 import { SideNav } from "../components/sideNav";
 import UserComponent from "../components/userComponent";
-import { activeState } from "../types/type";
-
+import { activeState, credentials } from "../types/type";
+import { useRouter } from "next/router";
 export const activeNavContext = createContext({
   sideNavOpen: false,
   activeNav: "",
@@ -15,8 +15,13 @@ export const activeNavContext = createContext({
 const Home: NextPage = () => {
   const [sideNavOpen, setSideNavOpen] = useState<boolean>(false);
   const [sideNavUserOpen, setSideNavUserOpen] = useState<boolean>(false);
-
   const [activeNav, setActiveNav] = useState<string>(activeState.DASHBOARD);
+  const [cred, setCred] = useState<credentials | string>((): any => {
+    if (typeof window !== "undefined")
+      return window.localStorage.getItem("creds");
+  });
+  const [loading, setLoading] = useState<boolean>(true);
+  const router = useRouter();
 
   const handleNav = () => {
     setSideNavOpen(!sideNavOpen);
@@ -26,35 +31,41 @@ const Home: NextPage = () => {
   };
 
   useEffect(() => {
-    console.log(activeNav);
-  }, [activeNav]);
+    !cred && router.push("/login");
+    cred && setLoading(false);
+    () => setLoading(false);
+  }, [cred, router]);
 
   return (
-    <activeNavContext.Provider
-      value={{ sideNavOpen, activeNav, sideNavUserOpen }}
-    >
-      <div className="max-h-screen bg-[#F6F6F9] overflow-hidden motion-reduce:transition-none motion-reduce:hover:transform-none">
-        <Navbar setNavOpen={handleNav} />
-        <div className="h-[calc(100vh-73px)] flex">
-          <SideNav
-            setNavOpen={handleNav}
-            onChangeActiveNav={(str: string) => {
-              handleActiveNav(str);
-            }}
-            onChangeUserNav={(arg) => setSideNavUserOpen(arg)}
-          />
-          <main className="p-5 py-6 h-full grow overflow-auto">
-            {activeNav === activeState.DASHBOARD ? (
-              <Dashboard />
-            ) : activeNav === activeState.SALES ? (
-              <SalesComponent />
-            ) : activeNav === activeState.USER_MNG ? (
-              <UserComponent />
-            ) : null}
-          </main>
-        </div>
-      </div>
-    </activeNavContext.Provider>
+    <>
+      {!loading && (
+        <activeNavContext.Provider
+          value={{ sideNavOpen, activeNav, sideNavUserOpen }}
+        >
+          <div className="max-h-screen bg-[#F6F6F9] overflow-hidden motion-reduce:transition-none motion-reduce:hover:transform-none">
+            <Navbar setNavOpen={handleNav} />
+            <div className="h-[calc(100vh-73px)] flex">
+              <SideNav
+                setNavOpen={handleNav}
+                onChangeActiveNav={(str: string) => {
+                  handleActiveNav(str);
+                }}
+                onChangeUserNav={(arg) => setSideNavUserOpen(arg)}
+              />
+              <main className="p-5 py-6 h-full grow overflow-auto">
+                {activeNav === activeState.DASHBOARD ? (
+                  <Dashboard />
+                ) : activeNav === activeState.SALES ? (
+                  <SalesComponent />
+                ) : activeNav === activeState.USER_MNG ? (
+                  <UserComponent />
+                ) : null}
+              </main>
+            </div>
+          </div>
+        </activeNavContext.Provider>
+      )}
+    </>
   );
 };
 

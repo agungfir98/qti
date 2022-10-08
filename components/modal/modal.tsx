2aa470b3477ef4239339142dae42f9f0d16b2296
Agung Firmansyah <agungfir98@gmail.com>
2022-10-08 19:17:04 +0700

    fix overflow

--- components/modal/modal.tsx
+++ components/modal/modal.tsx
@@ -11,12 +11,12 @@ const ModalContainer: React.FC<{
     <div
       className={`${
         !ModalOpen && "hidden"
-      } fixed z-10 top-0 left-0 h-screen w-screen overflow-auto bg-black/30 flex justify-center items-center`}
+      } fixed z-10 top-0 left-0 h-screen w-screen bg-black/30 flex justify-center items-center`}
       onClick={handleModalOpen}
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className="h-fit w-[90%] md:w-[50%] px-5 py-6 bg-white -mt-20 md:mt-0 overflow-auto rounded-xl relative"
+        className="h-fit max-h-[90%] w-[90%] md:w-[50%] px-5 py-6 bg-white -mt-20 md:mt-0 overflow-auto rounded-xl relative"
       >
         <div
           className="absolute font-semibold right-5 top-2 text-xl cursor-pointer"
